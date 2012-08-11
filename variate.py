fa064abd729fdcd81fb83d3c47dab98a65a5e74d
Michael N. Gagnon <mikegagnon@gmail.com>
2012-08-11 15:14:24 -0700

    fixed copyright typo

--- variate.py
+++ variate.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright 2012 Mike Gagnon, LLC
+# Copyright 2012 Mike Gagnon
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
